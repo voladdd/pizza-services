5b4b4209a3c736f472765927c5f2f6f9af57554e
Vlad <79471942+voladdd@users.noreply.github.com>
2023-02-02 19:18:10 +0300

    manager connected to mongodb

--- manager-server/src/index.ts
+++ manager-server/src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello");
+import { MongoClient } from "mongodb";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+//Mongo Setup
+const client = new MongoClient(process.env.MONGO_URI);
+
+async function run() {
+  try {
+    const database = client.db("slack_bot_db");
+    const orders = database.collection("orders_collection");
+    // const result = await orders.(order);
+    console.log(`A document was inserted with the _id: ${orders}`);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch((e) => console.log(e));
